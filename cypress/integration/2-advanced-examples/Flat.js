78162e110d9696ceb2daf8a69c1ea4b103daaf71
abhishekicha <apramanik28@gmail.com>
2022-08-09 14:47:55 +0530

    flats updated

--- cypress/integration/2-advanced-examples/Flat.js
+++ cypress/integration/2-advanced-examples/Flat.js
@@ -35,8 +35,157 @@ describe('Flats fuctionality', function(){
 
         cy.wait(2000)
 
-        
+        flatspage.getSearch().type('1A', {force: true})
+
+        cy.wait(2000)
+
+        flatspage.getVerifySearchResults().should('contain', '1A')
+
+        cy.wait(2000)
+
+        flatspage.getSearch().clear()
+
+        cy.wait(2000)
+
+        flatspage.getSearch().type('Aspen', {force: true})
+
+        cy.wait(2000)
+
+        flatspage.getVerifyFlatSearch().should('contain', 'Aspen')
+
+        cy.wait(2000)
+
+        flatspage.getSearch().clear()
+
+        cy.wait(2000)
+
+        flatspage.getCreateFlat().click({force: true})
+
+        cy.wait(2000)
+
+        flatspage.getFlatNo().type('1E', {force: true})
+
+        cy.wait(2000)
+
+        flatspage.getAreaInSqFt().type('1000', {force: true})
+
+        cy.wait(2000)
+
+        flatspage.getSelectFlatOwner().click({force: true})
+
+        cy.wait(2000)
+
+        flatspage.getSelectFlatOwnername().eq(3).click({force: true})
+
+        cy.wait(2000)
+
+        flatspage.getSelectBlock().click({force: true})
+
+        cy.wait(2000)
+
+        flatspage.getSelectBlockName().eq(21).click({force: true})
+
+        cy.wait(1000)
+
+        flatspage.getSelectFloor().click({force: true})
+
+        cy.wait(2000)
+
+        flatspage.getSelectExtensionNumber().type('101', {force: true})
+
+        cy.wait(2000)
+
+        flatspage.getSelectUnderMaintainanceBilling().check({force: true})
+
+        cy.wait(2000)
+
+        flatspage.getSelectKey().type('Test Flat', {force: true})
+
+        cy.wait(2000)
+
+        flatspage.getSelectSave().click({force: true})
+
+        cy.wait(2000)
 
     });
+
+    it('Flats Functionality With Negative Values', function(){
+
+      cy.login(this.data.email, this.data.password);
+
+      flatOwnersPage.getProperty().click()
+      
+      cy.wait(2000);
+
+      flatspage.getFlats().click()
+
+      cy.wait(2000)
+
+      flatspage.getSearch().type('..0..', {force: true})
+
+      cy.wait(2000)
+
+      flatspage.getSearch().clear()
+
+      cy.wait(2000)
+
+      flatspage.getSearch().type('..//', {force: true})
+
+      cy.wait(2000)
+
+      flatspage.getSearch().clear()
+
+      cy.wait(2000)
+
+      flatspage.getCreateFlat().click({force: true})
+
+      cy.wait(2000)
+
+      flatspage.getFlatNo().type('..0.*.', {force: true})
+
+      cy.wait(2000)
+
+      flatspage.getAreaInSqFt().type('...,,..', {force: true})
+
+      cy.wait(2000)
+
+      flatspage.getSelectFlatOwner().click({force: true})
+
+      cy.wait(2000)
+
+      flatspage.getSelectFlatOwnername().eq(3).click({force: true})
+
+      cy.wait(2000)
+
+      flatspage.getSelectBlock().click({force: true})
+
+      cy.wait(2000)
+
+      flatspage.getSelectBlockName().eq(21).click({force: true})
+
+      cy.wait(1000)
+
+      flatspage.getSelectFloor().click({force: true})
+
+      cy.wait(2000)
+
+      flatspage.getSelectExtensionNumber().type('..0..', {force: true})
+
+      cy.wait(2000)
+
+      flatspage.getSelectUnderMaintainanceBilling().check({force: true})
+
+      cy.wait(2000)
+
+      flatspage.getSelectKey().type('..**..',{force: true})
+
+      cy.wait(2000)
+
+      flatspage.getSelectSave().click({force: true})
+
+      cy.wait(2000)
+
+  });
+
 
 });
--- cypress/support/pageObjects/FlatsPage.js
+++ cypress/support/pageObjects/FlatsPage.js
@@ -11,7 +11,78 @@ class Flatspage{
 
     }
 
+    getVerifySearchResults(){
 
+        return cy.get('.tableRow > :nth-child(1) > [aria-colindex="2"]')
+    }
+
+    getVerifyFlatSearch(){
+
+        return cy.get('.tableRow > :nth-child(1) > [aria-colindex="3"]')
+    }
+
+    getCreateFlat(){
+
+        return  cy.get('.addButton')
+
+    }
+
+    getFlatNo(){
+
+        return cy.get('.row.mt-2 > :nth-child(1) input')
+    }
+
+    getAreaInSqFt(){
+
+        return cy.get('.row.mt-2 > :nth-child(2) input')
+    }
+
+    getSelectFlatOwner(){
+
+        return cy.get('.row.mt-2 > .col > .v-input > .v-input__control > .v-input__slot')
+    }
+
+    getSelectFlatOwnername(){
+
+        return cy.get('.v-list-item__title')
+
+    }
+
+    getSelectBlock(){
+
+        return cy.get(':nth-child(4) > .v-input > .v-input__control > .v-input__slot')
+    }
+
+    getSelectBlockName(){
+
+        return cy.get('.v-list-item__title')
+    }
+
+
+    getSelectFloor(){
+
+        return cy.get('.floatButt3')
+    }
+
+    getSelectExtensionNumber(){
+
+        return cy.get('.row.mt-2 > :nth-child(7) input')
+    }
+
+    getSelectUnderMaintainanceBilling(){
+
+        return cy.get("input[type='checkbox']")
+    }
+
+    getSelectKey(){
+
+        return cy.get('.material-input ')
+    }
+
+    getSelectSave(){
+
+        return cy.get('.text-center > .btn')
+    }
 }
 
 export default Flatspage;
